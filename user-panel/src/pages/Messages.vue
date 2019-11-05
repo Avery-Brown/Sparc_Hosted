abbb8f9a647f147577eedf53350bbde64c4264b7
VIRGO96 <dawoodahmed696@gmail.com>
2019-11-05 12:46:36 +0500

    blocking feedback

--- user-panel/src/pages/Messages.vue
+++ user-panel/src/pages/Messages.vue
@@ -134,147 +134,16 @@
         </div>
       </div>
     </div>
+      <b-modal id="modal-1" title="Feedback" @ok="sendBlockNotifiation">
+        <b-form-textarea
+          id="textarea"
+          v-model="block_message"
+          placeholder="Tell us why you are blocking this person "
+          rows="3"
+          max-rows="6"
+        ></b-form-textarea>
+      </b-modal>
   </div>
-        <!-- <div class="col-md-8">
-          <div class="container">
-              <div class="messaging">
-                    <div   :class="['inbox_msg' ,getClass]">
-                      <div class="inbox_people">
-                        <div class="headind_srch">
-                          <div class="recent_heading">
-                            <h4>Recent</h4>
-                          </div>
-                          <div class="srch_bar">
-                            <div class="stylish-input-group">
-                              <input type="text" class="search-bar"  placeholder="Search"  v-model="search" v-on:keydown="filter_name">
-                              <span class="input-group-addon">
-                              <button type="button"> <i class="fa fa-search" aria-hidden="true"></i> </button>
-                              </span> </div>
-                          </div>
-                        </div>
-                        <div class="inbox_chat">
-                          <div  class="chat_list active_chat " @click="fillProfile(users)" v-for="(users,i) in filters"  v-if="users.id!=lc_loggeduser.id" v-bind:key="i">
-                            <div class="chat_people" >
-                              <div class="chat_img"> <img class="rounded-circle" style="height:2rem;" :src="users.profile_image!=null ? users.profile_image: 'https://ptetutorials.com/images/user-profile.png'" alt="Anika"> </div>
-                              <div class="chat_ib">
-                                <h5>{{users.first_name}} <span class="chat_date">{{chatdate(users.id).date}}</span></h5>
-                                 <p>{{chatdate(users.id).message}}</p>
-                              </div>
-                            </div> 
-                          </div>
-                        </div>
-                      </div>
-                      <div class="mesgs">
-                        <div v-if="selected_messages.length>0" id="msg_containers" class="msg_history">
-                          <div  v-for="(items,i) in selected_messages" :key="i">
-                            <div v-if="items.sender_id!=lc_loggeduser.id" class="incoming_msg mt-2">
-                            <div class="received_msg">
-                              <div class="received_withd_msg">
-                                <p>{{items.message}}</p>
-                                <sub v-if="items.type!=null"> <a v-if="items.type!=null" target="_blank" :href="items.file_url">View Here</a> </sub>
-                                <span class="time_date"> {{items.date}}</span></div>
-                            </div>
-                          </div>
-                          <div v-else class="outgoing_msg">
-                            <div class="sent_msg mr-1">
-                              <p>{{items.message}}</p>
-                                <sub v-if="items.type!=null"> <a v-if="items.type!=null" target="_blank" :href="items.file_url">View Here</a> </sub>
-                              <span class="time_date"> {{items.date}}</span> </div>
-                          </div>
-                          </div>
-                        </div>
-                        <div v-else class="msg_history">
-                            <p>No Messages with this user</p>
-                        </div>
-                        <div class="type_msg">
-                          <div class="input_msg_write">
-                            <input type="text" class="write_msg ml-1 mr-1" style="border:none !important;" @keyup.enter="sendMessage()" v-model="message" placeholder="Type a message" />
-                            <button class="msg_send_btn"  @click="sendMessage()" type="button"><i class="fa fa-paper-plane-o" aria-hidden="true"></i></button>
-                            <button class="file_send_btn"  @click="onPickFile()"  type="button"><i class="fa fa-paperclip" aria-hidden="true"></i></button>
-                            <input type="file" style="display:none;" @change="onFilePicked" ref="FileInput" accept="images/*" >
-                             <span @click="rawfile=null" class="fa fa-trash  btn btn-primary" v-if="rawfile!=null"> {{rawfile[0].name}}</span> 
-                          </div>
-                        </div>
-                      </div>
-                    </div> 
-              </div>
-            </div>
-        </div> -->
-        <!-- <div class="col-md-4">
-           <b-card    
-            tag="article"
-            style="max-width: 30rem;-webkit-box-shadow:none;border:1px solid #dcdcdc"
-            class="mb-2">
-            <template v:slot="header">
-              <h4 class="text-center"> <b> About </b></h4>
-            </template>
-            <b-card-body>
-              <div class="photo-container text-center" >
-                
-                <img class="pic"  :src="selected_user.profile_image!=null ? selected_user.profile_image :'https://ptetutorials.com/images/user-profile.png'" alt="" />
-              </div>
-              <div class="row">
-                <div class="col-md-12">
-                  <h6 class="users text-center">{{selected_user.first_name}}</h6>
-                </div>
-                <div class="col-md-12">
-                  <h6 class="users text-muted text-center" style="margin-top:-15px;">{{selected_user.job_occupation}}</h6>
-                </div>
-
-
-              </div>
-              <div class="row text-center">
-                <div class="col-md-3">
-                  <span>D.O.B</span>
-                </div>
-                <div class="col-md-9">
-                  <span ><strong>{{selected_user.age}}</strong></span>
-                </div>
-              </div>
-              <div class="row text-center mt-4">
-                <div class="col-md-3">
-                  <span>Email</span>
-                </div>
-                <div class="col-md-9">
-                  <span ><strong>{{selected_user.email}}</strong></span>
-                </div>
-              </div>
-              <div class="row text-center mt-4">
-                <div class="col-md-4">
-                  <span>Institution</span>
-                </div>
-                <div class="col-md-8">
-                  <span ><strong>{{selected_user.institute}}</strong></span>
-                </div>
-              </div>
-              <div class="row text-center mt-4">
-                <div class="col-md-4">
-                  <span>Block User</span>
-                </div>
-                <div class="col-md-8">
-                  <b-form-checkbox v-model="checked" @change="blocking()" name="check-button" switch>
-                  </b-form-checkbox>
-                </div>
-              </div>
-              <div class="row text-center mt-4">
-                <div class="col-md-5">
-                  <span>All Conversations</span>
-                </div>
-                <div class="col-md-6">
-                  <b-form-checkbox @change="toggleEmailNotifs()" v-model="notif_toggle"  name="check-button" switch>
-                  </b-form-checkbox>
-                </div>
-              </div>
-
-            </b-card-body>
-            
-          </b-card>
-        </div> -->
-      <!-- </div>
-    </div>
-
-    
-  </div> -->
 </template>
 <script>
 import { mapGetters, mapActions } from 'vuex'
@@ -290,6 +159,7 @@ export default {
   },
   data(){
     return{
+      block_message:'',
       rawfile:null,
       notif_toggle:false,
       search:'',
@@ -397,12 +267,32 @@ export default {
     blocking(){
       const remove_blocker_index = this.allUsers.findIndex(user_item => user_item.id == this.selected_user.id);
       if(!this.checked==false){
-      //removing blocked ids from the state
+      // removing blocked ids from the state
       this.$store.commit('removeBlocker',{key:this.get_receiver_blocked_status.key,index:remove_blocker_index})
-      //removing block ids from currently selectd user
+      // removing block ids from currently selectd user
       delete this.selected_user.blocked_by[this.get_receiver_blocked_status.key]
       }
+      else{
+        this.$bvModal.show('modal-1')
+      }
       this.blockingProcess({receiver_id:this.selected_user.id,sender_id:this.lc_loggeduser.id,flag:!this.checked})
+    
+    },
+    sendBlockNotifiation(){
+      if(this.block_message!='') {
+          console.log(this.selected_user.first_name+''+this.block_message+''+this.lc_loggeduser.first_name)
+        axios.post('https://us-central1-sparc-9d9cb.cloudfunctions.net/blockMessageNotification', {
+          blocked_user:this.selected_user.first_name,
+          message: this.block_message,
+          blocked_by:this.lc_loggeduser.first_name,
+        }).then(() => {
+          console.log('Email Sent')
+        }).catch(err => console.log("Error " + err))
+      }
+      else{
+        console.log("notifiers")
+      }
+      
     },
     sendMessage(){
       if(this.get_my_block_status==true){
