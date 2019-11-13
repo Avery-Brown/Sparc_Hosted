fe0bdcaef8a6001bcc45170c483c2e83c2100615
Eric <el172@duke.edu>
2019-11-13 01:17:04 -0500

    added test and fixed babel hopefully

--- user-panel/src/tests/example.spec.js
+++ user-panel/src/tests/example.spec.js
@@ -1,9 +1,17 @@
-const mount = require('@vue/test-utils').mount;
-const Contact = require('../pages/Contact.vue')
+import { mount } from '@vue/test-utils'
+import Contact from '../pages/Contact'
+import About from '../pages/About'
 
 describe('Contact', () => {
-    test('is a Vue instance', () => {
-      const wrapper = mount(Contact)
-      expect(wrapper.isVueInstance()).toBeTruthy()
-    })
+  test('Contact is a Vue instance', () => {
+    const wrapper = mount(Contact)
+    expect(wrapper.isVueInstance()).toBeTruthy()
   })
+})
+
+describe('About', () => {
+  test('About is a Vue instance yea', () => {
+    const wrapper = mount(About)
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+})
