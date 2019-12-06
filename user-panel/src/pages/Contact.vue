d4ef6992f8f3bfcd92d164dede9d617b9529cb2c
LIZZET <lc244@duke.edu>
2019-12-06 14:23:21 -0500

    changing contact us page

--- user-panel/src/pages/Contact.vue
+++ user-panel/src/pages/Contact.vue
@@ -31,8 +31,8 @@ export default {
             <div class = "row">
                 <div class = "container">
                     <div class="col-md-9 ml-auto mr-auto">
-                        
-                            
+
+
                         <Card class="contact-card">
                             <b-alert variant="danger" v-model="warning" dismissible style="margin-bottom: -0.5rem">
                                     <div class="alert-icon">
@@ -40,8 +40,8 @@ export default {
                                     </div>
                                     <strong>Something went wrong!</strong> {{ message }}
                                 </b-alert>
-                          
-                        
+
+
                                 <b-alert variant="success" v-model="success" dismissible style="margin-bottom: -0.5rem">
                                     <div class="alert-icon">
                                     <i class="now-ui-icons objects_support-17"></i>
@@ -53,15 +53,15 @@ export default {
                             <p style= "color: #484848; font-weight: 400; font-size: 15px">Have a question or just want to chat? Get in contact below</p>
                             <fg-input id="login_input" style="border-color: black;" class="input-lg" addon-left-icon="now-ui-icons emoticons_satisfied" placeholder="Name" v-model="userPayload.name" type="name"></fg-input>
                             <fg-input id="login_input" class="input-lg" addon-left-icon="now-ui-icons ui-1_email-85" placeholder="Email" v-model="userPayload.email" type="email"></fg-input>
-                            <!-- <b-form-textarea class="message-box input-lg" placeholder="Message" v-model="userPayload.message" type="message"></b-form-textarea> -->
+                            <b-form-textarea style="height: 500px;" placeholder="Message" v-model="userPayload.message" type="message"></b-form-textarea>
 
                             <!-- <div id="app">
                               <froala id="edit" :tag="'textarea'" :config="config" v-model="model"></froala>
                             </div> -->
 
-                            <div id="app">
+                            <!-- <div id="app">
                               <froala id="edit" placeholder="Message" :tag="'textarea'" v-model="userPayload.message" type="message"></froala>
-                            </div>
+                            </div> -->
 
                             <div class="card-footer text-center">
 
@@ -75,7 +75,7 @@ export default {
                     </div>
                 </div>
             </div>
-            
+
         </div>
     </div>
 </template>
